7009cbc6cbf3d7a0f1cd0175581d333198085c9a
yihuan.lin <yihuan.lin@yahutech.com>
2024-06-14 17:04:06 +0800

    fix:拔掉input阻擋enter功能

--- vite-project/src/components/ui/input.tsx
+++ vite-project/src/components/ui/input.tsx
@@ -7,12 +7,8 @@ export interface InputProps
 
 const Input = React.forwardRef<HTMLInputElement, InputProps>(
   ({ className, type, ...props }, ref) => {
-    const checkKeyDown = (e: React.KeyboardEvent<HTMLInputElement | HTMLTextAreaElement>) => {
-      if (e.key === 'Enter') e.preventDefault();
-    };
     return (
-      <input
-        onKeyDown={checkKeyDown}
+      <input       
         type={type}
         className={cn(
           "flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-slate-50 file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-offset-gray-500 focus-visible:ring-offset-1 disabled:cursor-not-allowed disabled:opacity-50",
